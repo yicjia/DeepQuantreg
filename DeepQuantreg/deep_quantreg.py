3b0c39b79ad40f74f72dc8e116b998226d834d4f
Yichen <58962571+yicjia@users.noreply.github.com>
2021-03-25 22:42:00 -0400

    Update deep_quantreg.py

--- DeepQuantreg/deep_quantreg.py
+++ DeepQuantreg/deep_quantreg.py
@@ -48,11 +48,13 @@ def deep_quantreg(train_df,test_df,layer=2,node=300,n_epoch=100,bsize=64,acfn="s
     E_train = train_df["E"]
     W_train = train_df["W"]
     n1 = np.shape(Y_train)[0]
+    u1 = np.max(Y_train)[E_train==1]
     
     X_test = test_df["X"]
     Y_test = test_df["Y"]
     E_test = test_df["E"]
     n2 = np.shape(Y_test)[0]
+    u2 = np.max(Y_train)[E_train==1]
     
     
     model = Sequential()
@@ -72,7 +74,7 @@ def deep_quantreg(train_df,test_df,layer=2,node=300,n_epoch=100,bsize=64,acfn="s
     Qpred = np.reshape(Qpred, n1)
     ci = get_ci(Y_train,Qpred,E_train)
     mse = get_mse(np.log(Y_train),np.log(Qpred),E_train)
-    ql = get_ql(np.log(Y_train),np.log(Qpred),E_train,np.log(186))
+    ql = get_ql(np.log(Y_train),np.log(Qpred),E_train,np.log(u1))
     
     if uncertainty==False: 
         Qpred2 = np.exp(model.predict(X_test))
@@ -84,7 +86,7 @@ def deep_quantreg(train_df,test_df,layer=2,node=300,n_epoch=100,bsize=64,acfn="s
     
     ci2 = get_ci(Y_test,Qpred2,E_test)
     mse2 = get_mse(np.log(Y_test),np.log(Qpred2),E_test)
-    ql2 = get_ql(np.log(Y_test),np.log(Qpred2),E_testnp.log(186))
+    ql2 = get_ql(np.log(Y_test),np.log(Qpred2),E_test,np.log(u2))
         
     print( 'Concordance Index for training dataset:', ci)
     print( 'Concordance Index for test dataset:', ci2)
